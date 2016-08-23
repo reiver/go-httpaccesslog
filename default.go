775a17089f89413863c604840b5c6db125f560a4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-08-23 15:30:33 -0700

    updated default logging format, for HTTP access log.

--- default.go
+++ default.go
@@ -16,45 +16,81 @@ var (
 
 func DefaultAccessLogWriter(writer io.Writer, w *ResponseWriter, r *http.Request, trace *Trace) {
 
-	io.WriteString(writer, "remote-host=")
+	io.WriteString(writer, "\"remote-host\"=")
 	io.WriteString(writer, strconv.Quote(r.RemoteAddr) ) //@TODO: This is inefficient.
 
 	writer.Write(space)
 
-	io.WriteString(writer, "request-method=")
+	io.WriteString(writer, "\"request-method\"=")
 	io.WriteString(writer, strconv.Quote(r.Method))
 
 	writer.Write(space)
 
-	io.WriteString(writer, "request-uri=")
+	io.WriteString(writer, "\"request-uri\"=")
 	io.WriteString(writer, strconv.Quote(r.URL.String())) //@TODO: This is inefficient.
 
 	writer.Write(space)
 
-	io.WriteString(writer, "request-protocol=")
+	io.WriteString(writer, "\"request-protocol\"=")
 	io.WriteString(writer, strconv.Quote(r.Proto)) //@TODO: This is inefficient.
 
 	writer.Write(space)
 
-	io.WriteString(writer, "response-status-code=\"")
+	io.WriteString(writer, "\"response-status-code\"=\"")
 	io.WriteString(writer, strconv.FormatInt(int64(w.StatusCode), 10)) //@TODO: This is inefficient.
 	io.WriteString(writer, "\"")
 
 	writer.Write(space)
 
-	io.WriteString(writer, "response-size=\"")
+	io.WriteString(writer, "\"response-size\"=\"")
 	io.WriteString(writer, strconv.FormatInt(int64(w.BodySize), 10)) //@TODO: This is inefficient.
 	io.WriteString(writer, "\"")
 
 	writer.Write(space)
 
-	io.WriteString(writer, "trace.begin-time=")
+	io.WriteString(writer, "\"trace\".\"begin-time\"=")
 	io.WriteString(writer, strconv.Quote(trace.BeginTime.String())) //@TODO: This is inefficient.
 
 	writer.Write(space)
 
-	io.WriteString(writer, "trace.end-time=")
+	io.WriteString(writer, "\"trace\".\"end-time\"=")
 	io.WriteString(writer, strconv.Quote(trace.EndTime.String())) //@TODO: This is inefficient.
+
+	for headerName, headerValues := range r.Header {
+
+		writer.Write(space)
+
+		io.WriteString(writer, "\"header\".")
+		io.WriteString(writer, strconv.Quote(headerName)) //@TODO: This is inefficient.
+		io.WriteString(writer, "=")
+
+		io.WriteString(writer, "[")
+		for i, headerValue := range headerValues {
+			if 0 < i {
+				io.WriteString(writer, ", ")
+			}
+			io.WriteString(writer, strconv.Quote(headerValue)) //@TODO: This is inefficient.
+		}
+		io.WriteString(writer, "]")
+	}
+
+	for trailerName, trailerValues := range r.Trailer {
+
+		writer.Write(space)
+
+		io.WriteString(writer, "\"trailer\".")
+		io.WriteString(writer, strconv.Quote(trailerName)) //@TODO: This is inefficient.
+		io.WriteString(writer, "=")
+
+		io.WriteString(writer, "[")
+		for i, trailerValue := range trailerValues {
+			if 0 < i {
+				io.WriteString(writer, ", ")
+			}
+			io.WriteString(writer, strconv.Quote(trailerValue)) //@TODO: This is inefficient.
+		}
+		io.WriteString(writer, "]")
+	}
 
 	writer.Write(endl)
 }
