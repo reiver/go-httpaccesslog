afcb1520aa8b67b6e427adbb5da4e8564cc4c85b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-10-03 09:43:17 -0700

    added some docs

--- doc.go
+++ doc.go
@@ -1,6 +1,16 @@
 /*
 Package httpaccesslog provides HTTP "middleware" that provides access log generation capabilities.
 
+Simple Example:
+
+	var subhandler http.Handler
+	
+	// ...
+	
+	var httpOverlordHandler http.Handler = httpaccesslog.Handler{
+		Subhandler: subhandler,
+		Writer:     os.Stdout,
+	}
 
 */
 package httpaccesslog
